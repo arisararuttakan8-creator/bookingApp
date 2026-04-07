cc6e53e48968513bad1131698d5f580a678abb20
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-07 10:07:17 +0700

    connect mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+import dotenv from "dotenv";
 import express from "express"
-const app = express();
+import mongoose from "mongoose";
 
+
+const app = express();
+dotenv.config()
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect( process.env.MONGO);
+        console.log('connected to mongoDB.')
+    } catch (error) {
+        throw error
+    }  
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log('mongoDB disconnected!')
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log('mongoDB connected!')
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log("MongoDB error:", err);
+  });
 app.listen(8800,()=>{
+    connect()
     console.log('connected to backend.')
 })

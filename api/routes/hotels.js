f1c156f56b182d9b127676eaeae7b2ee6e94ae33
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-07 14:32:52 +0700

    hotels api put delete getbyid getall

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,8 +16,49 @@ router.post('/', async( req , res)=>{
 
 })
 //UPDATE
+router.put('/:id', async( req , res)=>{
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate( req.params.id , {$set:req.body} , {new:true})
+        res.status(200).send( updatedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
 //DELETE
+router.delete('/:id', async( req , res)=>{
+    try{
+        await Hotel.findByIdAndDelete( req.params.id )
+        res.status(200).send( 'Hotel has been deleted.')
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
 //GET 
+router.get('/:id', async( req , res)=>{
+    try{
+        const hotel = await Hotel.findById( req.params.id )
+        res.status(200).send( hotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
 //GET ALL
+router.get('/', async( req , res)=>{
+    try{
+        const hotels = await Hotel.find()
+        res.status(200).send( hotels)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
 
 export default router;

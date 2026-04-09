f636507f0a0f3c39629b0f99bfaeb61d83da874e
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-09 14:46:17 +0700

    use data at propetyList.jsx , add countHotelByType fuction

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -3,11 +3,12 @@ import axios from "axios"
 
 const useFetch = (url) =>{
     const [data,setData] = useState([])
-    const [loading,setLoading] = useState(false)
+    const [loading,setLoading] = useState(true)
     const [error,setError] = useState(false)
 
     useEffect(()=>{
         const fetchData = async ()=>{
+            setLoading(true)
             try{
                 const res = await axios.get(url)
                 setData( res.data )

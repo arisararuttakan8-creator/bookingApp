5121133420829274b29f2f2e5d5d7bd94852a297
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-16 16:54:43 +0700

    SearchContext

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -3,7 +3,6 @@ import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
   const {data, loading , error } = useFetch('/hotels?featured=true&limit=4')
-  console.log('data: ', data)
   return (
     <div className="fp">
       {loading ? 'loading' : 

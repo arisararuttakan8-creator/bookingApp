bf8051df2542f2b13d7f4bd93c3b895dac685271
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-18 09:40:57 +0700

    remove login/register button after the user is authenticated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,18 +1,22 @@
+import { useContext } from "react"
 import "./navbar.css"
 import {Link} from "react-router-dom"
+import {AuthContext} from "../../context/AuthContext"
+
 const Navbar = () => {
+  const { user } = useContext(AuthContext)
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
           <span className="logo">lamabooking</span>
         </Link>
-        <div className="navItems">
+       { user ? user.userName :  (<div className="navItems">
           <button className="navButton">Register</button>
           <Link to="/login">
             <button className="navButton" >Login</button>
           </Link>
-        </div>
+        </div>)}
       </div>
     </div>
   )

d528e4a4297515633aeb75b7e3b9a0ab85567c36
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-04-28 17:39:07 +0700

    fix warning

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -26,7 +26,7 @@ const Hotel = () => {
   const [openModal, setOpenModal] = useState(false);
 
 
-  const { data , loading , error } = useFetch( `/hotels/find/${id}`,)
+  const { data , loading } = useFetch( `/hotels/find/${id}`,)
   const { user } = useContext( AuthContext )
   const navigate = useNavigate()
 

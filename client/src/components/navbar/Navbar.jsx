643d77468ae6204487a85b47c183fff568d369c7
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-05-29 09:11:39 +0700

    feat(FE):Logout

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,16 +2,22 @@ import { useContext } from "react"
 import "./navbar.css"
 import {Link} from "react-router-dom"
 import {AuthContext} from "../../context/AuthContext"
+import UserDropdown from "../userDropdown/UserDropdown"
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext)
+  const { user ,dispatch } = useContext(AuthContext)
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
           <span className="logo">arisbooking</span>
         </Link>
-       { user ? user.userName :  (<div className="navItems">
+       { user ? (
+                <UserDropdown 
+                    user={user}        
+                    dispatch={dispatch} 
+                />
+            ) :  (<div className="navItems">
           <Link to="/register">
             <button className="navButton">Register</button>
           </Link>
